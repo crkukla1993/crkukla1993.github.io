7ad8a72b5b86eeab70796125bab00f979a20a08b
Christopher Kukla <crkukla1993@gmail.com>
2017-01-09 18:57:19 -0600

    Improved page flow

--- js/custom.js
+++ js/custom.js
@@ -1,7 +1,8 @@
 $(document).ready(function(){
+	var h = $(window).height();
 	$('.header-title')
 	.animate({opacity: 'show'}, {queue: false, duration: 1500})
-	.animate({marginTop: '19%'}, {duration: 750, complete: function(){
+	.animate({marginTop: h*.3}, {duration: 750, complete: function(){
 		$('.s1')
 		.css({opacity: 0.0, visibility: "visible", marginTop: '-1%'}).animate({opacity: 1}, 
 		{queue: false, duration: 500, complete: function(){
@@ -18,65 +19,86 @@ $(document).ready(function(){
 			}});
 		}});
 	}});
-	
+	if(h < 480){
+		h = 480;
+	}
 	$('.blur').width($(window).width());
-	$('.blur').height($(window).height());
+	$('.blur').height(h);
 	
 	$('.header-div').width($(window).width());
-	$('.header-div').height($(window).height());
+	$('.header-div').height(h);
 		
 	$('#aboutme').width($(window).width());
-	$('#aboutme').height($(window).height());
+	$('#aboutme').height(h);
 		
 	$('#portfolio').width($(window).width());
-	$('#portfolio').height($(window).height());
+	$('#portfolio').height(h);
 		
 	$('#skills').width($(window).width());
-	$('#skills').height($(window).height());
+	$('#skills').height(h);
+	
+	$('#aboutmebtn').mouseenter(function(){
+		var n = 0;
+	});
 	
 	$('#aboutmebtn').click(function(){
-		$('html body').animate({scrollTop: $("#aboutme").position().top}, 1000);
+		$('html body').animate({scrollTop: $("#aboutme").position().top}, 1500);
 	});
 	$('#portfoliobtn').click(function(){
-		$('html body').animate({scrollTop: $("#portfolio").position().top}, 1000);
+		$('html body').animate({scrollTop: $("#portfolio").position().top}, 1500);
 	});
 	$('#skillsbtn').click(function(){
-		$('html body').animate({scrollTop: $("#skills").position().top}, 1000);
+		$('html body').animate({scrollTop: $("#skills").position().top}, 1500);
 	});
 	
-	var loopBounce = setInterval(function() {
-		$('#downarrow').effect('bounce', {times: 3}, 500);
-	}, 4000);
+	var bounced = false;
 	
-	$(document).scroll(function(){
-		var i = $('html body').scrollTop();
-		if(i === 0){
-			$('#downarrow').animate({opacity: 'show'}, 500);
-			loopBounce = setInterval(function() {
-				$('#downarrow').effect('bounce', {times: 3}, 500);
-			}, 4000);
+	t = setTimeout(function(){
+		var n = $('#downarrow').css('opacity');
+		if($('#downarrow').css("opacity") !== '0' && !bounced){
+			$('#downarrow').effect('bounce', {times: 3}, 500);
+			bounced = true;
+		}
+	}, 8000);
+	
+	$(window).scroll(function(){
+		if($('html body').scrollTop() === 0){
+			$('#downarrow').animate({opacity: 1}, {queue: false, duration: 300});
+		
+			t = setTimeout(function(){
+				if($('#downarrow').css("opacity") !== '0' && !bounced){
+					$('#downarrow').effect('bounce', {times: 3}, 500);
+					bounced = true
+				}
+			}, 8000);
 		}
 		else{
-			clearInterval(loopBounce);
-			$('#downarrow').animate({opacity: 'hide'}, 500);
+			clearTimeout(t);
+			bounced = false;
+			$('#downarrow').animate({opacity: 0}, {queue: false, duration: 300});
 		}
 	});
-
-}); 
+});
 
 $(window).resize(function(){
+	var h = $(window).height();
+	if(h < 480){
+		h = 480;
+	}
+	$('.header-title').css('marginTop', h*.3)
 	$('.blur').width($(window).width());
-	$('.blur').height($(window).height());
+	$('.blur').height(h);
 	
 	$('.header-div').width($(window).width());
-	$('.header-div').height($(window).height());
+	$('.header-div').height(h);
 		
 	$('#aboutme').width($(window).width());
-	$('#aboutme').height($(window).height());
+	$('#aboutme').height(h);
 		
 	$('#portfolio').width($(window).width());
-	$('#portfolio').height($(window).height());
+	$('#portfolio').height(h);
 		
 	$('#skills').width($(window).width());
-	$('#skills').height($(window).height());
+	$('#skills').height(h);
+	
 });
